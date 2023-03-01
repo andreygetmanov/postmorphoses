56adb5e9d6dd1179f14c65500142570aeea9f8ec
andreygetmanov <andrey.getmanov2802@gmail.com>
2023-03-01 20:17:18 +0400

    - txt2voice changed to free pyttsx3 - offensive words filter added - political censorship added))0 - background color of webpage changed to black

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import pandas as pd
+import re
 
 from scripts.model import GPTModel
 from scripts.speech import SpeechGenerator
 from scripts.manifest import Manifest
+
+
+def offensive_filter(text_to_clean: str, offensive_words: list) -> str:
+    swear_words = ['((х|x)(у|y)(й|е|ё|и|я|ли[^а-я]|э))',
+                   '(п(и|е|ё)(з|с)д)', '([^а-я])(би?ля(д|т|[^а-я]))',
+                   '(пид(о|а)р|п(е|и)дри)', '(муд(ак|ач|о|и))',
+                   '(([^а-я]|по|на|от|не|ни)(х|x)(е|e)(р|p))', '(з(а|о)луп(а|и))',
+                   '(([^а-я]у?|под?|на|за|от|вы|ь|ъ)(е|ё|и)б(а|ыр|у|нут|ись|ище))',
+                   '([^а-я])((на|по)х)([^а-я])', '(pizd)', '(п(е|ё)рд(а|у|и|е|ё))'] + offensive_words
+    pattern = re.compile('|'.join(swear_words), re.IGNORECASE)
+    clean_text = pattern.sub('', text_to_clean)
+    return clean_text
+
+
+def censorship(text_to_clean: str) -> str:
+    words_to_del = ['путин', 'хох', 'украин', 'войн', 'террор', 'правительств']
+    pattern = re.compile('|'.join(words_to_del), re.IGNORECASE)
+    clean_text = pattern.sub('у меня нет слов', text_to_clean)
+    return clean_text
 
 
 def get_data(path: str):
@@ -13,17 +33,18 @@ def get_data(path: str):
 
 if __name__ == '__main__':
     texts = get_data('data/ruscorpora_content.xlsx')
+    offensive_words = open('data/offensive_words.txt', 'r', encoding='utf-8').read().split(', ')
     text_file = 'data/text_for_gen.txt'
     manifest = Manifest(pause=0.07)
     history = manifest.read_txt(text_file)
     GPTModel = GPTModel()
-    SpeechGenerator = SpeechGenerator(folder_id='b1g7khuono0tf56uljpm', voice='alena')
+    SpeechGenerator = SpeechGenerator()
     while True:
         for text in texts:
             new_text = manifest.read_txt(text_file)
             if new_text != history:
                 text = new_text if new_text else text
                 history = text
-            gen_text = GPTModel.generate(text)
+            gen_text = censorship(offensive_filter(GPTModel.generate(text), offensive_words))
             SpeechGenerator.play(gen_text)
             manifest.print_one_by_one(gen_text)
--- scripts/speech.py
+++ scripts/speech.py
@@ -1,41 +1,28 @@
-import requests
 import sounddevice
 import soundfile
+import pyttsx3
 
 
 class SpeechGenerator:
 
     def __init__(self,
-                 folder_id: str,
                  path: str = './audio.raw',
-                 api_token: str = None,
-                 voice: str = 'alena',
-                 lang: str = 'ru-RU',
-                 format: str = 'lpcm',
-                 sample_rate_hz: int = 48000,
+                 sample_rate_hz: int = 19200,
                  channels: int = 1,
                  subtype: str = 'pcm_16'
                  ):
         """
         :param path: path to created file
-        :param folder_id: YandexCloud folder id
-        :param api_token: YandexCloud auth token
-        :param voice: voice type
-        :param lang: language
-        :param format: format of synthesized data
         :param sample_rate_hz: sample rate in Hz
         :param channels: number of channels
         :param subtype: subtype of the sound file
         """
         self.path = path
-        self.folder_id = folder_id
-        self.api_token = api_token if api_token else self._get_api_token()
-        self.voice = voice
-        self.lang = lang
-        self.format = format
         self.sample_rate_hz = sample_rate_hz
         self.channels = channels
         self.subtype = subtype
+        self.tts = pyttsx3.init()
+        self.voices = self.tts.getProperty('voices')
 
     def play(self, text: str) -> None:
         """
@@ -43,42 +30,9 @@ class SpeechGenerator:
         :param text: text for synthesis
         :return: plays audio
         """
-        with open(self.path, "wb") as f:
-            for audio_content in self._synthesize(text=text):
-                f.write(audio_content)
-        audiodata, samplerate = soundfile.read(self.path, channels=self.channels,
-                                               samplerate=self.sample_rate_hz, subtype=self.subtype)
-        sounddevice.play(audiodata)
-
-    def _synthesize(self, text: str) -> bytes:
-        """
-        Synthesizes speech (in binary format) from the input string of text.
-        :param text: text for synthesis
-        :return: array of bytes
-        """
-        url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
-        headers = {
-            'Authorization': 'Api-Key ' + self.api_token,
-        }
-
-        data = {
-            'text': text,
-            'folderId': self.folder_id,
-            'voice': self.voice,
-            'lang': self.lang,
-            'format': self.format,
-            'sampleRateHertz': self.sample_rate_hz
-        }
-
-        with requests.post(url, headers=headers, data=data, stream=True) as resp:
-            if resp.status_code != 200:
-                raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
-
-            for chunk in resp.iter_content(chunk_size=None):
-                yield chunk
-
-    @staticmethod
-    def _get_api_token() -> str:
-        with open('scripts/auth.txt', 'r') as f:
-            token = f.read().split(':')[1]
-        return token
+        self.tts.setProperty('voice', 'ru')
+        self.tts.save_to_file(text, self.path)
+        self.tts.runAndWait()
+        audiodata, samplerate = soundfile.read(self.path, samplerate=self.sample_rate_hz,
+                                               channels=self.channels, subtype=self.subtype)
+        sounddevice.play(audiodata, samplerate=samplerate)
